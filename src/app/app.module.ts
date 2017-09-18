61a0fa80531574b75f46400862fe8fa215c2d3ab
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-18 19:26:48 +0530

    Basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DataStructureComponent } from './data-structure/data-structure.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataStructureComponent
   ],
   imports: [
     BrowserModule
