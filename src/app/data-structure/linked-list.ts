972d3c1c9a00533a021c3a9ab24683763ec65934
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-18 19:35:26 +0530

    Minor changes

--- src/app/data-structure/linked-list.ts
+++ src/app/data-structure/linked-list.ts
@@ -1,18 +1,17 @@
 import { Node } from './node';
 
 export interface LinkedList<T> {
-    addFirst(data : T)
-    addLast(data : T)
-    addBefore(data : T, key : T)
-    addAfter(data : T, key : T)
+    addFirst(data: T)
+    addLast(data: T)
+    add(data: T, pos: number)
 }
 
 export class SinglyLinkedList<T> implements LinkedList<T> {
-    private head : Node<T>
+    private head: Node<T>
 
-    addFirst(data : T) { this.head = new Node(data, this.head) }
+    addFirst(data: T) { this.head = new Node(data, this.head) }
 
-    addLast(data : T) {
+    addLast(data: T) {
         if (this.head == null) this.addFirst(data)
         else {
             let temp = this.head
@@ -20,7 +19,8 @@ export class SinglyLinkedList<T> implements LinkedList<T> {
             temp.next = new Node(data)
         }
     }
-    
-    addBefore(data : T, key : T) {}
-    addAfter(data : T, key : T) {}
+
+    add(data: T, pos: number) {
+
+    }
 }
--- src/app/data-structure/node.ts
+++ src/app/data-structure/node.ts
@@ -1,22 +1,22 @@
 export class Node<T> {
-    private _data : T
-    private _next : Node<T>
-    private _prev : Node<T>
+    private _data: T
+    private _next: Node<T>
+    private _prev: Node<T>
 
-    constructor(data: T, next? : Node<T>, prev? : Node<T>) {
+    constructor(data: T, next?: Node<T>, prev?: Node<T>) {
         this._data = data
         this._next = next
         this._prev = prev
     }
 
-    get data() : T { return this._data }
-    set data(data : T) { this._data = data }
-    
-    get next() : Node<T> { return this._next }
-    set next(next : Node<T>) { this._next = next }
+    get data(): T { return this._data }
+    set data(data: T) { this._data = data }
 
-    get prev() : Node<T> { return this._prev }
-    set prev(prev : Node<T>) { this._prev = prev }
+    get next(): Node<T> { return this._next }
+    set next(next: Node<T>) { this._next = next }
 
-    public toString() : string { return this._data.toString() }
+    get prev(): Node<T> { return this._prev }
+    set prev(prev: Node<T>) { this._prev = prev }
+
+    public toString(): string { return this._data.toString() }
 }
