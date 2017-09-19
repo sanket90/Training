d7db853a0ac7020b1bac568682b16d51f495d95b
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-19 19:25:17 +0530

    Bug fix

--- src/app/example/postfix.ts
+++ src/app/example/postfix.ts
@@ -18,36 +18,28 @@ export class Postfix {
     }
 
     private do = (char) => {
-        switch (char) {
-            case util.isOperand(char):
-                this.postfix.concat(char);
-                break;
+        if (util.isOperand(char)) { this.appendToResult(char) }
 
-            case util.isOpeningBracket(char):
-                this.stack.push(char);
-                break;
+        else if (util.isOpeningBracket(char)) { this.stack.push(char) }
 
-            case util.isClosingBracket(char):
-                this.processClosingBracket();
-                break;
+        else if (util.isClosingBracket(char)) { this.processClosingBracket() }
 
-            default:
-                this.processOperator(char)
-                break;
-        }
+        else { this.processOperator(char) }
     }
 
     private processClosingBracket() {
-        while (!this.stack.isEmpty() && !util.isOpeningBracket(this.stack.peek())) this.postfix.concat(this.stack.pop())
+        while (!this.stack.isEmpty() && !util.isOpeningBracket(this.stack.peek())) this.appendToResult(this.stack.pop())
         this.stack.pop()
     }
 
     private processOperator(char) {
         while (!this.stack.isEmpty() && util.precedence(char) <= util.precedence(this.stack.peek())) {
-            this.postfix.concat(this.stack.pop())
+            this.appendToResult(this.stack.pop())
         }
         this.stack.push(char)
     }
 
-    private popUntillNotEmptry() { while (!this.stack.isEmpty()) this.postfix.concat(this.stack.pop()) }
+    private popUntillNotEmptry() { while (!this.stack.isEmpty()) this.appendToResult(this.stack.pop()) }
+
+    private appendToResult(char) { this.postfix = this.postfix + char }
 }
