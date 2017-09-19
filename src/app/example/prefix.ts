bd780b54be3718acc909261eaa6a5f45587c4f02
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-19 18:17:01 +0530

    Prefix implementations

--- src/app/example/prefix.ts
+++ src/app/example/prefix.ts
@@ -1,3 +1,28 @@
+import { Postfix } from './postfix';
+
 class Prefix {
-    
+    fromInfixToPostfix(infix: string): string {
+        let reverseInfix = this.reverse(infix)
+        let eqArr = this.toArray(infix)
+        eqArr.forEach(char => this.do(char));
+        reverseInfix = eqArr.join()
+
+        let postfix = new Postfix().fromInfixToPostfix(reverseInfix)
+
+        let prefix = this.reverse(postfix)
+        return prefix
+    }
+
+    private do = (char) => {
+        if (this.isOpeningBracket(char)) char = ")"
+        if (this.isClosingBracket(char)) char = "("
+    }
+
+    private reverse(str) { return str.split('').reverse().join('') }
+
+    private toArray(eqn: string) { return Array.from(eqn) }
+
+    private isOpeningBracket(char): boolean { return char === "(" }
+
+    private isClosingBracket(char): boolean { return char === ")" }
 }
