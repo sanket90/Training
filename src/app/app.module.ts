97d426715fd702ad7362608dfc8066335565c654
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-19 14:01:07 +0530

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DataStructureComponent } from './data-structure/data-structure.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataStructureComponent
   ],
   imports: [
     BrowserModule
