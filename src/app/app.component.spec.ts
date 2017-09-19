c9d995570b9edf462033873fb18bddf8d6190b72
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-19 19:26:06 +0530

    Tests written for Stack, Prefix, Postfix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,46 @@
+import { ListStack, LinkedListStack, ArrayStack } from './data-structure/stack';
+import { Postfix } from './example/postfix';
+import { Prefix } from './example/prefix';
+
 import { TestBed, async } from '@angular/core/testing';
 
-import { AppComponent } from './app.component';
-
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it('should check array stack', async(() => {
+    let stack = new ArrayStack<string>()
+    stack.push("abc")
+    expect(stack.peek()).toBe("abc")
+    expect(stack.pop()).toBe("abc")
+    expect(stack.isEmpty()).toBe(true)
+  }))
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+  it('should check linked list stack', async(() => {
+    let stack = new LinkedListStack<string>()
+    stack.push("abc")
+    expect(stack.peek()).toBe("abc")
+    expect(stack.pop()).toBe("abc")
+    expect(stack.isEmpty()).toBe(true)
+  }))
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
+  it('should check list stack', async(() => {
+    let stack = new ListStack<string>()
+    stack.push("abc")
+    expect(stack.peek()).toBe("abc")
+    expect(stack.pop()).toBe("abc")
+    expect(stack.isEmpty()).toBe(true)
+  }))
+
+  it('should check infix to postfix', async(() => {
+    expect((new Postfix()).fromInfix("a+b")).toBe("ab+")
+    expect((new Postfix()).fromInfix("a+b*c+d")).toBe("abc*+d+")
+    expect((new Postfix()).fromInfix("(a+b)/(c*d)-(e+f)")).toBe("ab+cd*/ef+-")
+    expect((new Postfix()).fromInfix("(c+d)-(a*b)/(a+c)")).toBe("cd+ab*ac+/-")
+  }))
+
+  it('should check infix to prefix', async(() => {
+    expect((new Prefix()).fromInfix("a+b")).toBe("+ab")
+    expect((new Prefix()).fromInfix("a+b*c+d")).toBe("+a+*bcd")
+    expect((new Prefix()).fromInfix("(a+b)/(c*d)-(e+f)")).toBe("-/+ab*cd+ef")
+    expect((new Prefix()).fromInfix("(c+d)-(a*b)/(a+c)")).toBe("-+cd/*ab+ac")
+  }))
 });
